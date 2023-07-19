6d21f4f09142256836c534a9b78b0f4745d1b5f9
shiri2001 <Shiri@shirile01@gmail.com>
2023-07-19 17:35:54 +0300

    style: added dots to end of sentences

--- infrastructure/modules/basic-vpc-infrastructure/variables.tf
+++ infrastructure/modules/basic-vpc-infrastructure/variables.tf
@@ -19,26 +19,26 @@ variable "project" {
 
 variable "az" {
   type        = list(string)
-  description = "The aws availability zones"
+  description = "The aws availability zones."
   default     = ["eu-west-1a", "eu-west-1b"]
 
 }
 
 variable "private_subnet_cidr_blocks" {
   type        = list(string)
-  description = "A list of the cidr blocks used by the private subnets"
+  description = "A list of the cidr blocks used by the private subnets."
   default     = ["10.0.0.0/20", "10.0.16.0/20"]
 }
 
 variable "public_subnet_cidr_blocks" {
   type        = list(string)
-  description = "A list of the cidr blocks used by the public subnets"
+  description = "A list of the cidr blocks used by the public subnets."
   default     = ["10.0.32.0/20", "10.0.48.0/20"]
 }
 
 variable "allowed_cidr_block_access" {
   type        = string
-  description = "The destination cidr block to the internet"
+  description = "The destination cidr block to the internet."
   default     = "0.0.0.0/0"
 }
 
